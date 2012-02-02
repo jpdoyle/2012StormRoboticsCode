eed53d2decb826289f5eda2f5c97d7f971e57802
Vishnu Kadirisani <610112@lrstudents.org>
2012-02-02 17:27:53 -0500

    Added DriveTrain Module

---
+++ src/storm/modules/DriveTrain.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package storm.modules;
+import edu.wpi.first.wpilibj.*;
+import storm.interfaces.IDriveTrain;
+
+/**
+ *
+ * @author Storm
+ */
+public class DriveTrain implements IDriveTrain {
+
+    SpeedController leftMotor1, leftMotor2, rightMotor1, rightMotor2;
+    
+    public DriveTrain (int motorChannelL1,int motorChannelL2,int motorChannelR1,int motorChannelR2 ){
+        leftMotor1 = new Victor(motorChannelL1);
+        leftMotor2 = new Victor(motorChannelL2);
+        rightMotor1 = new Victor(motorChannelR1);
+        rightMotor2 = new Victor(motorChannelR2);
+    }
+    
+    public void drive(double leftSpeed, double rightSpeed) {
+        leftMotor1.set(leftSpeed);
+        leftMotor2.set(leftSpeed);
+        rightMotor1.set(rightSpeed);
+        rightMotor2.set(rightSpeed);
+    }
+    
+}
