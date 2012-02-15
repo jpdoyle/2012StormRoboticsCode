5841d1e91e20133c0d3a6bec27f7f4884b2c2b92
Joe Doyle (Ginto8) <615714@lrstudents.org>
2012-02-15 16:37:11 -0500

    Fixed TrackerTest

--- src/storm/logic/TrackerTest.java
+++ src/storm/logic/TrackerTest.java
@@ -5,10 +5,10 @@
 
 package storm.logic;
 
-import edu.wpi.first.wpilibj.Gyro;
-import edu.wpi.first.wpilibj.RobotDrive;
+import storm.RobotState;
 import storm.interfaces.IDriveTrain;
 import storm.interfaces.IRobotLogic;
+import storm.modules.DriveTrain;
 import storm.modules.TargetTracker;
 import storm.utility.Print;
 
@@ -17,19 +17,15 @@ import storm.utility.Print;
  * @author joe
  */
 public class TrackerTest implements IRobotLogic {
-    TargetTracker tracker;
-    Print print;
-
-    public TrackerTest(IDriveTrain drive,Gyro gyro) {
-        tracker = new TargetTracker(drive,gyro);
-    }
-
+    Print print = Print.getInstance();
+    IDriveTrain drive = new DriveTrain(RobotState.PORT_MOTOR_DRIVE_LEFT,RobotState.PORT_MOTOR_DRIVE_RIGHT);
+    TargetTracker tracker = new TargetTracker(drive,RobotState.PORT_GYRO_ROBOT_ROTATION);
+    
     public void doInit() {
         tracker.startTracking();
     }
 
     public void doContinuous() {
-        print = Print.getInstance();
         boolean aimed = tracker.isAimed();
         print.println(aimed ? "Aimed" : "Not Aimed");
         if(aimed)
