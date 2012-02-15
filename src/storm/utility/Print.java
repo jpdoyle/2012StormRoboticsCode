074f4dc49db8e0924cd01567687bee32629de421
Joe Doyle <615714@lrstudents.org>
2012-02-15 15:51:22 -0500

    Updated Print class

--- src/storm/utility/Print.java
+++ src/storm/utility/Print.java
@@ -1,79 +1,121 @@
 package storm.utility;
 
 import edu.wpi.first.wpilibj.DriverStationLCD;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
 
 public class Print {
+    private static Print instance = null;
 
-	private static DriverStationLCD driver = DriverStationLCD.getInstance();
-	private static String[] line;
+    public static Print getInstance() {
+        if(instance == null)
+            instance = new Print();
+        return instance;
+    }
 
-	public Print(){
-		line = new String[6];
-		for(int i = 0; i < line.length; i++) {
-                    line[i] = "";
-		}
-	}
+    private static final int
+            NUMLINES = 6,
+            NUMCOLUMNS = 21;
+    private static final String CLEARLINE = "                     ";
 
-	public void setLine(int lineNumber, String inputString)
-	{
-		//sets user's input to a desired line number
-		line[lineNumber] = inputString;
-		update();
-	}
+	private DriverStationLCD lcd_ = DriverStationLCD.getInstance();
+	private String[] lines_ = new String[NUMLINES];
 
-	public void printLine(String inputString)
-	{
-		//Prints Text from top to bottom
-		printText(inputString);
-		update();
-	}
+    private Print() {
+        clearScreen();
+    }
 
-	private void printText(String str) {
-		//Alternate Code: bottom to top; needs tweaking
-		//for(int i = 0; i < line.length; i++)
-		for(int i = line.length-1; i > 0; i--)
-		{
-			line[i] = line[i-1];
-		}
-		line[0] = str;
-	}
+    public final void clearLine(int which) {
+        lines_[which] = CLEARLINE;
+        update();
+    }
 
-	public void clearScreen()
-	{
-		for(int i = 0; i < line.length; i++) {
-                    line[i] = "                                    ";
-		}
-	}
+    public final void clearScreen() {
+        for(int i=0;i<lines_.length;++i)
+            lines_[i] = CLEARLINE;
+        update();
+    }
 
-	private void update() {
-		for(int i = 0; i < line.length; i++)
-		{
-                    DriverStationLCD.Line driverLine = null;
-                    switch (i) {
-                        case 0:
-                            driverLine = DriverStationLCD.Line.kMain6;
-                            break;
-                        case 1:
-                            driverLine = DriverStationLCD.Line.kUser2;
-                            break;
-                        case 2:
-                            driverLine = DriverStationLCD.Line.kUser3;
-                            break;
-                        case 3:
-                            driverLine = DriverStationLCD.Line.kUser4;
-                            break;
-                        case 4:
-                            driverLine = DriverStationLCD.Line.kUser5;
-                            break;
-                        case 5:
-                            driverLine = DriverStationLCD.Line.kUser6;
-                            break;
-                        default:
-                            driverLine = DriverStationLCD.Line.kMain6;
-                            break;
-                    }
-                    driver.println(driverLine, 1, line[i]);
-		}
-		driver.updateLCD();
-	}
+    private static String padString(String s) {
+        if(s.length() >= NUMCOLUMNS)
+            return s.substring(0,NUMCOLUMNS);
+        return s + CLEARLINE.substring(s.length());
+    }
+
+    public final void setLine(int which,String s) {
+        lines_[which] = padString(s);
+        update();
+    }
+
+    private static boolean isEmpty(String s) {
+        return s.equals(CLEARLINE);
+    }
+
+    private void rotateUp(int start,int end,String lastLine) {
+        for(int i=start;i<end-1;++i) {
+            lines_[i] = lines_[i+1];
+        }
+        lines_[end-1] = lastLine;
+    }
+
+    private void println(int start,int end,String s) {
+        if(end > lines_.length)
+            end = lines_.length;
+        if(start < 0)
+            start = 0;
+        if(start > end)
+            return;
+        s = padString(s);
+        int i;
+        for(i=end-1;i>=start && isEmpty(lines_[i]);--i) {}
+        ++i;
+        if(i == end)
+            rotateUp(start,end,s);
+        else
+            lines_[i] = s;
+        update();
+    }
+
+    public final void println(String s) {
+        println(0,lines_.length,s);
+    }
+
+    private class ostream_ extends OutputStream {
+        String buffer_ = "";
+        int start_,end_;
+
+        public ostream_(int start,int end) {
+            start_ = start;
+            end_ = end;
+        }
+
+        public void write(int b) throws IOException {
+            if(b == '\n') {
+                println(start_,end_,buffer_);
+                buffer_ = "";
+                return;
+            }
+            buffer_ += (char)b;
+        }
+
+    }
+
+    public final PrintStream getPrintStream(int start,int end) {
+        return new PrintStream(new ostream_(start,end));
+    }
+
+    public final PrintStream getPrintStream() {
+        return getPrintStream(0,lines_.length);
+    }
+
+    private void update() {
+        lcd_.println(DriverStationLCD.Line.kMain6, 1, lines_[0]);
+        lcd_.println(DriverStationLCD.Line.kUser2, 1, lines_[1]);
+        lcd_.println(DriverStationLCD.Line.kUser3, 1, lines_[2]);
+        lcd_.println(DriverStationLCD.Line.kUser4, 1, lines_[3]);
+        lcd_.println(DriverStationLCD.Line.kUser5, 1, lines_[4]);
+        lcd_.println(DriverStationLCD.Line.kUser6, 1, lines_[5]);
+        lcd_.updateLCD();
+    }
 }
