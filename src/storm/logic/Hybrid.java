0818c654d2430a6fdafb671dee804150eb3aa48f
unknown <Developer@FRC2012DS.(none)>
2012-02-16 20:09:57 -0500

    Beginning of hybrid

--- src/storm/logic/Hybrid.java
+++ src/storm/logic/Hybrid.java
@@ -4,7 +4,11 @@
  */
 package storm.logic;
 
+import edu.wpi.first.wpilibj.AnalogChannel;
+import storm.RobotState;
+import storm.interfaces.IDriveTrain;
 import storm.interfaces.IRobotLogic;
+import storm.utility.Queue;
 
 /**
  *
@@ -12,16 +16,103 @@ import storm.interfaces.IRobotLogic;
  */
 public class Hybrid implements IRobotLogic {
 
+    IDriveTrain driveTrain;
+
+    AnalogChannel autoType;
+
+    Queue Q = new Queue();
+
+    public Hybrid(int port) {
+
+        driveTrain = RobotState.driveTrain;
+
+        autoType = new AnalogChannel(port);
+
+    }
+
     public void doInit() {
+
+        //1 - Straight    \\
+        //2 - Left        \\
+        //3 - Load        \\
+        //4 - Shoot       \\
+        //5 - Manipulator \\
+
+        switch (autoType.getValue()) {
+            case 1: //Super Auto Mode
+
+                //Aim
+                //Shoot
+                Q.add(1, 60, -.5);
+                Q.add(5, 25, 1);
+                //Pick up
+                //Possibly move
+                //Aim
+                //Shoot
+
+                break;
+            case 2: //Quick ninja
+
+                Q.add(1, 60, -.5);
+                Q.add(5, 25, 1);
+                //Possibly do a shoot sequence
+
+                break;
+            case 3: //Hit and run
+
+                //Aim
+                //Shoot
+
+                break;
+            case 4: //Flee in terror to the left
+
+                Q.add(2, 50, .8);
+                Q.add(1, 50, .5);
+
+                break;
+            case 5: //Flee in terror to the right
+
+                Q.add(2, 50, -.8);
+                Q.add(1, 50, .5);
+
+                break;
+            case 6: //Sitting duck
+                break;
+            default:
+                break;
+        }
+
+        Q.start();
+
     }
 
     public void doContinuous() {
+        //Eat a muffin.  please :)
     }
 
     public void doPeriodic() {
+
+        if (Q.isRunning()) runQueue();
+        if (driveTrain.getDistance() >= Q.getDistance()) Q.next();
+
     }
 
     public void doEnd() {
+    }
+
+    public void runQueue() {
+
+        switch ((int)Q.getType()) {
+            case 1:
+                driveTrain.drive(Q.getSpeed(), Q.getSpeed());
+                break;
+            case 2:
+                driveTrain.drive(Q.getSpeed(), -Q.getSpeed());
+                break;
+            default:
+                break;
+        }
+
     }
     
 }
