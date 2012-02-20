cbc1610f465c1054c644beb672b1be18b1db1dfa
Greg Herman <130312@lrstudents.org>
2012-02-20 12:01:14 -0500

    Or maybe not

--- src/storm/modules/Shooter.java
+++ src/storm/modules/Shooter.java
@@ -17,18 +17,23 @@ import storm.utility.Print;
  */
 
 public class Shooter implements IShooter {
+
+    //Joystick shootJoystick;
     SpeedController shooterMotor, transferMotor;
     DigitalInput ready, hallEffect;
     Counter counter;
+    Joystick shootJoystick;
     boolean shooting,
 	    readyTripped;
+    boolean btn7;
     double motorSpeed,
 	    calculatedMotorSpeed,
 	    wantedRPM,
 	    period,
 	    RPMcurrent,
 	    RPMdifference,
-	    RPMthreshold;
+	    RPMthreshold,
+	    RPMold;
     int state,
 	    timeDifference,
 	    currentTime,
@@ -49,6 +54,8 @@ public class Shooter implements IShooter {
     }
     
     public void startShoot(double distance) {
+	shootJoystick = RobotState.joystickShoot;
+
 	motorSpeed = getMotorSpeed(distance);
         counter.start();
         state = 0;
@@ -63,6 +70,31 @@ public class Shooter implements IShooter {
     long startTime = -1;
 
     public void doShoot() {
+	/*if (!shooting) return;
+	checkRPM();
+	transferMotor.set(-1);
+	if (shootJoystick.getRawButton(7) && !btn7) {
+	    btn7 = true;
+	    state ++;
+	    if (state> 3) state = 0;
+	    switch (state){
+		case 0: motorSpeed = getMotorSpeed(2.5);		
+		    break;
+		case 1: motorSpeed = getMotorSpeed(5);
+		    break;
+		case 2: motorSpeed = getMotorSpeed(3);
+		    break;
+		case 3: motorSpeed = getMotorSpeed(7);
+		    break;
+	    }
+	    shooterMotor.set(motorSpeed);
+	    startTime = System.currentTimeMillis();
+
+	} else if (!shootJoystick.getRawButton(7) && btn7) {
+	    btn7 = false;
+	}
+	Print.getInstance().setLine(2, "Motor Speed: " + motorSpeed);*/
+
         // set motor speed, check when ready, move ball into shooter, stop once IR sensor is clear
 	if (!shooting) return;
 	switch (state){
@@ -91,12 +123,15 @@ public class Shooter implements IShooter {
 		}
 		break;
 	    case 4:
-		if ((System.currentTimeMillis() - startTime) >= 2000){
+		if ((System.currentTimeMillis() - startTime) >= 3000){
 		    state ++;
 		}
 		break;
 	    case 5:
-		endShoot();
+		transferMotor.set(0);
+		shooterMotor.set(0);
+		shooting = false;
+		state = 0;
 		RobotState.BALL_CONTAINMENT_COUNT --;
 		break;
 	    default:
@@ -108,6 +143,7 @@ public class Shooter implements IShooter {
         //convert distance into rpm into motor speed value
 	wantedRPM = 333.33*distance + 850.63 ;
 	calculatedMotorSpeed = .0003*wantedRPM + 0.0457;
+	Print.getInstance().setLine(3, "Wanted RPM: " + wantedRPM);
 	return calculatedMotorSpeed;
     }
     
@@ -115,6 +151,8 @@ public class Shooter implements IShooter {
 	//check what the current RPM is
 	
 	period = counter.getPeriod();
+	Print.getInstance().setLine(5, "Ctr:" + counter.get() + "Prd:" + period);
+	//System.out.println("Prd:" + period);
 	if ((System.currentTimeMillis() - startTime) >= 10000)
 	{
 	    return true;
@@ -127,6 +165,7 @@ public class Shooter implements IShooter {
 	
 	if (Double.isInfinite(period) || period <= 0)
 	{
+	    System.out.println("Infinite, period: " + period);
 	    return false;
 	}
 	
@@ -135,22 +174,30 @@ public class Shooter implements IShooter {
 	if (RPMcurrent > 1200) modFactor = 5;
 	else modFactor = 10;
 	
-	RPMthreshold = wantedRPM / 100;	
-	RPMdifference = wantedRPM - RPMcurrent;
+	RPMthreshold = wantedRPM / 100;
 	
+	Print.getInstance().setLine(1, "RPM: " + RPMcurrent);
+	Print.getInstance().setLine(4, "RPM difference: " + RPMdifference);
+	Print.getInstance().setLine(0, "???????");
+	
+	RPMdifference = wantedRPM - RPMcurrent;
 	motorSpeed += .00003*RPMdifference;
 	
 	if (motorSpeed <0) motorSpeed = 0;
 	if (motorSpeed >1) motorSpeed = 1;
 	
 	shooterMotor.set(motorSpeed);
+	
+	Print.getInstance().setLine(0, "motor speed-: " + motorSpeed);
 	    	
 	if (Math.abs(RPMdifference) < RPMthreshold)
 	{
 	    goodRangeCount ++;
 	}else goodRangeCount = 0;
-	
-	if(goodRangeCount > 10)
+	//System.out.println("goodRangeCount:" + goodRangeCount);
+	System.out.println("RPMW:" + wantedRPM + " RPMC: " + RPMcurrent + " RPMD: " + RPMdifference + " MTRSpd: " + motorSpeed + " PRD: " + period);	
+
+	if(goodRangeCount > 30)
 	{
 	    return true;
 	}else return false;
@@ -161,7 +208,7 @@ public class Shooter implements IShooter {
 	transferMotor.set(0);
 	state = 0;
 	shooting = false;
-	debugCounter = 0;
+	
     }
 
     public boolean isShooting() {
