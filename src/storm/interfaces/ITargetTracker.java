fa829c064fdeddbd9e0188e879d27b4127e1e549
David Korhumel <135712@lrstudents.org>
2012-02-20 14:17:27 -0500

    Added isLocking()

--- src/storm/interfaces/ITargetTracker.java
+++ src/storm/interfaces/ITargetTracker.java
@@ -4,6 +4,7 @@ public interface ITargetTracker {
     
     public void startLocking();
     public void stopLocking();
+    public boolean isLocking();
     public void startTracking();
     public void stopTracking();
     public boolean isAimed();
