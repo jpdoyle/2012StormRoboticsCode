502e420fedb54801af7767cba86a8242358c9d6e
Joe Doyle (Ginto8) <615714@lrstudents.org>
2012-02-21 16:52:46 -0500

    Added FancyBinaryImage functionality to prevent TargetTracker crashes

--- src/edu/wpi/first/wpilibj/image/FancyBinaryImage.java
+++ src/edu/wpi/first/wpilibj/image/FancyBinaryImage.java
@@ -10,7 +10,18 @@ package edu.wpi.first.wpilibj.image;
  * @author joe
  */
 public class FancyBinaryImage extends BinaryImage {
+    private int numParticles;
     public FancyBinaryImage() throws NIVisionException {
         super();
+    }
+
+    public int getNumberParticles () throws NIVisionException{
+        numParticles = NIVision.countParticles(image);
+        return numParticles;
+    }
+
+    public ParticleAnalysisReport getParticleAnalysisReport(int index) throws NIVisionException{
+        if(index < numParticles) throw new IndexOutOfBoundsException("Invalid particle index");
+        return new ParticleAnalysisReport(this, index);
     }
 }
